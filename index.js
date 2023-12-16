e9e8c4fb7474a088cb5c1e147586e93c7ad5f9df
Shengjie <n9.shengjie@gmail.com>
2023-12-15 19:35:48 -0500

    Updated the Index

--- index.js
+++ index.js
@@ -1,48 +1,30 @@
-const bodyParser = require('body-parser')
-const express = require("express")
-const app = express()
-const port = 4000
+const bodyParser = require('body-parser');
+const express = require('express');
+const app = express();
+const port = 4000;
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-// this allows us to use all files in the public folder
-app.use(express.static(__dirname + '/public'))
+app.get('/', (req, res) => {
+    res.sendFile('public/Zach Content/players_page.html', { root: __dirname });
+});
 
-// GET, which serves the welcome_page.html page
-app.get("/", (req, res) => {
-    res.sendFile("public/Zach Content/welcome_page.html", { root: __dirname })
-})
+app.get('/about', (req, res) => {
+    res.sendFile('public/About.html', { root: __dirname });
+});
 
-// GET, which serves the players.html page
-app.get("/", (req, res) => {
-    res.sendFile("public/Zach Content/players_page.html", { root: __dirname })
-})
+app.get('/home', (req, res) => {
+    res.sendFile('public/homePage.html', { root: __dirname });
+});
 
-// GET, which serves the About.html page
-app.get("/catalog", (req, res) => {
-    res.sendFile("public/About.html", { root: __dirname })
-})
+app.get('/help', (req, res) => {
+    res.sendFile('public/Help.html', { root: __dirname });
+});
 
-// GET, which serves the homePage.html page
-app.get("/catalog", (req, res) => {
-    res.sendFile("public/homePage.html", { root: __dirname })
-})
-
-// GET, which serves the Help.html page
-app.get("/catalog", (req, res) => {
-    res.sendFile("public/Help.html", { root: __dirname })
-})
-
-// GET, which serves the catalog.html page
-app.get("/catalog", (req, res) => {
-    res.sendFile("public/team.html", { root: __dirname })
-})
-
-// example GET endpoint, which returns the current date
-app.get("/current_date", (req, res) => {
-    res.send(new Date())
-})
+app.get('/team', (req, res) => {
+    res.sendFile('public/team.html', { root: __dirname });
+});
 
 app.listen(port, () => {
-    console.log('APP is running')
-})
+    console.log(`Server is running on http://localhost:${4000}`);
+});
